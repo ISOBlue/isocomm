1f58b989d844830454c0d0b109b0dd578e50f3b9
ssabpisa <ssabpisa@me.com>
2013-10-25 15:31:15 -0400

    Initial commit

---
+++ gen/edu/purdue/isocomm/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.purdue.isocomm;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/purdue/isocomm/Map.java
@@ -0,0 +1,22 @@
+package edu.purdue.isocomm;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Map extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.map, menu);
+		return true;
+	}
+
+}
