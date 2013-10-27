88abce46ed40eec14cc1910bdb2cc9e7822a5872
ssabpisa <ssabpisa@me.com>
2013-10-27 01:06:37 -0400

    Eclipse built-in git is terrible

--- src/edu/purdue/isocomm/Map.java
+++ src/edu/purdue/isocomm/Map.java
@@ -14,6 +14,7 @@ import com.google.android.gms.maps.model.PolygonOptions;
 import android.os.Bundle;
 import android.app.Activity;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Menu;
 
 public class Map extends Activity {
